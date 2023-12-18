9c58af5134570e8e8f3eac3f9d29fb8792dcdb7f
jaydess21 <98706895+jaydess21@users.noreply.github.com>
2023-12-17 21:50:56 -0500

    Start of website

---
+++ src/login/signin.js
@@ -0,0 +1,13 @@
+// SignIn.js
+import React from 'react';
+
+const SignIn = () => {
+  return (
+    <div>
+      <h2>Sign In Page</h2>
+      <p>Sign in to access your account.</p>
+    </div>
+  );
+};
+
+export default SignIn;
